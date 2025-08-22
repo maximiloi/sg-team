6052ae6d828c4f9a16ae0df2a1023219dff801f1
maximiloi <maximiloi@gmail.com>
2025-08-22 12:04:20 +0300

    feat: add walpapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,25 @@ import {
 export default function Home() {
   return (
     <main className='flex items-center justify-center min-h-screen'>
-      <Card className='w-full max-w-[22rem]'>
+      <div
+        className='fixed top--5 left-1 -translate-x-1/4 -translate-y-1/4'
+        style={{
+          width: '290vw', // шире экрана, чтобы компенсировать поворот
+          height: '210vh', // выше экрана
+          backgroundImage: "url('/logo.png')",
+          backgroundRepeat: 'repeat',
+          backgroundSize: '250px 250px',
+          transform: 'rotate(-45deg)',
+          transformOrigin: 'center center',
+          zIndex: 0,
+          position: 'fixed',
+        }}
+      />
+      <Card className='w-full max-w-[22rem] relative z-10'>
         <CardHeader>
           <CardTitle className='text-3xl'>Мы обновляемся для вас!</CardTitle>
           <CardDescription className='text-lg'>
-            Остались вопросы? Заполните форму, и мы обязательно вам перезвоним!
+            Заполните форму, и мы обязательно вам перезвоним!
           </CardDescription>
         </CardHeader>
         <CardContent>
