f19165a9c6a6d393185e4865c12155f66c173485
maximiloi <maximiloi@gmail.com>
2025-08-26 18:45:11 +0300

    fix: cwd path

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: 'site-sgt',
       script: 'npm',
       args: 'start',
-      cwd: '/var/www/sg-team',
+      cwd: '/var/www/sgt',
       env: {
         NODE_ENV: 'production',
         PORT: 3002,
@@ -14,7 +14,7 @@ module.exports = {
       name: 'telegram-bot-sgt',
       script: 'bot/bot.js',
       interpreter: 'node',
-      cwd: '/var/www/sg-team',
+      cwd: '/var/www/sgt',
       env: {
         TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
         TELEGRAM_MANAGER_IDS: process.env.TELEGRAM_MANAGER_IDS,
