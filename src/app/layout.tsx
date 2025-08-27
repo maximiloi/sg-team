62e8a903c1bd0095887005350d3a4ee31a05d9a4
maximiloi <maximiloi@gmail.com>
2025-08-27 10:28:27 +0300

    feat: add metadata + font, fix size logo images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,82 @@ import type { Metadata } from 'next';
 import { Inter, Rubik } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['cyrillic'],
-});
-
 const rubik = Rubik({
   variable: '--font-rubik',
   subsets: ['cyrillic'],
+  display: 'swap',
+});
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['cyrillic'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: 'Автосервис - Super Garage Team',
-  description: 'Автосервис команды Super Garage Team',
+  title: 'Автосервис - Super Garage Team | TO за час',
+  description:
+    'Профессиональный автосервис Super Garage Team. Ремонт и обслуживание автомобилей любых марок в короткие сроки с гарантией качества. ТО за час',
+  keywords: [
+    'автосервис',
+    'ремонт авто',
+    'СТО',
+    'автомастерская',
+    'Super Garage Team',
+    'обслуживание автомобилей',
+    'диагностика авто',
+    'ремонт двигателя',
+    'замена масла',
+    'шиномонтаж',
+  ],
+  authors: [{ name: 'Super Garage Team', url: 'https://sgt-garage.ru' }],
+  creator: 'Super Garage Team',
+  publisher: 'Super Garage Team',
+  applicationName: 'Super Garage Team',
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    title: 'Автосервис - Super Garage Team',
+    description:
+      'Надёжный автосервис с гарантией качества. Ремонт и обслуживание автомобилей любых марок.',
+    url: 'https://sgt-garage.ru',
+    siteName: 'Super Garage Team',
+    images: [
+      {
+        url: '/logo.png', // сюда лучше положить картинку в public/
+        width: 250,
+        height: 250,
+        alt: 'Super Garage Team - автосервис',
+      },
+    ],
+    locale: 'ru_RU',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Автосервис - Super Garage Team',
+    description:
+      'Профессиональный ремонт и обслуживание автомобилей. Super Garage Team.',
+    images: ['/logo.png'],
+  },
+  category: 'Автосервис',
+  alternates: {
+    canonical: 'https://sgt-garage.ru',
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
