5404e1794519253df518d113233b4e4aca36c9b6
maximiloi <maximiloi@gmail.com>
2025-08-28 13:45:03 +0300

    config: add network path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['http://10.0.1.9:3000/'],
 };
 
 export default nextConfig;
