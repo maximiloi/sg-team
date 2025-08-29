af5d77cdb34405180cda4eef6a3ec0c9a0cc0492
maximiloi <maximiloi@gmail.com>
2025-08-29 11:50:27 +0300

    feat: add sidebar menu template

---
+++ src/components/SidebarMenuNav.tsx
@@ -0,0 +1,61 @@
+import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react';
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from './ui/sidebar';
+
+const items = [
+  {
+    title: 'Главная',
+    url: '/board',
+    icon: Home,
+  },
+  {
+    title: 'Заявки',
+    url: '#',
+    icon: Inbox,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+  {
+    title: 'Search',
+    url: '#',
+    icon: Search,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+];
+
+export default function SidebarMenuNav() {
+  return (
+    <>
+      <SidebarGroup>
+        <SidebarGroupLabel>Super Garage Team</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </>
+  );
+}
