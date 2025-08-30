7e60a06451757be27b07561e09af08d33e805cfb
maximiloi <maximiloi@gmail.com>
2025-08-30 12:35:24 +0300

    refactor: getRequestsByStatus

--- src/app/actions/requests.ts
+++ src/app/actions/requests.ts
@@ -1,9 +1,18 @@
 'use server';
 
+import { RequestStatus } from '@/generated/prisma';
 import { prisma } from '@/lib/prisma';
 
 export async function getRequests() {
   return prisma.request.findMany({
+    orderBy: { createdAt: 'asc' },
+    include: { client: true },
+  });
+}
+
+export async function getRequestsByStatus(status: RequestStatus) {
+  return prisma.request.findMany({
+    where: { status },
     orderBy: { createdAt: 'asc' },
     include: { client: true },
   });
