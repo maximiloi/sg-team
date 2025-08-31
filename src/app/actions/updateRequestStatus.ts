73560538766fb01cdc4ff3ee15d82a47510c432d
maximiloi <maximiloi@gmail.com>
2025-08-31 21:05:03 +0300

    feat: add action request status

---
+++ src/app/actions/updateRequestStatus.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import { RequestStatus } from '@/generated/prisma';
+import { prisma } from '@/lib/prisma';
+import { redirect } from 'next/navigation';
+import { auth } from '../auth/authSetup';
+
+export async function updateRequestStatus(
+  requestId: number,
+  status: RequestStatus
+) {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    throw new Error('Пользователь не авторизован');
+  }
+
+  // обновляем заявку
+  const updated = await prisma.request.update({
+    where: { id: requestId },
+    data: {
+      status,
+      updatedById: session.user.id,
+    },
+  });
+
+  // пишем в историю
+  await prisma.actionLog.create({
+    data: {
+      action: `Изменение статуса на ${status}`,
+      requestId: requestId,
+      userId: session.user.id,
+    },
+  });
+
+  redirect(`/board/request/${requestId}`);
+
+  return updated;
+}
