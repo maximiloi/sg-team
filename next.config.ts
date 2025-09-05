1bbc02126e45821ba2faccd105f2cdc332ad6c52
maximiloi <maximiloi@gmail.com>
2025-09-05 12:04:40 +0300

    fix: address

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['http://10.0.1.9:3000/'],
+  allowedDevOrigins: ['10.0.1.9'],
 };
 
 export default nextConfig;
