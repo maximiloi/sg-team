3a0eefae4f09ab4c2b16345aca3e13926f6dcef8
maximiloi <maximiloi@gmail.com>
2026-02-17 13:48:04 +0300

    feat: add component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import AdvantageList from '@/components/AdvantageList';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import PricesBlock from '@/components/PricesBlock';
+import WhatWeFix from '@/components/WhatWeFix';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className='md:flex md:items-center md:justify-center'>
+      <main className='md:flex md:items-center md:justify-center md:flex-col'>
         <Hero />
+        <AdvantageList />
+        <WhatWeFix />
+        <PricesBlock />
       </main>
     </>
   );
