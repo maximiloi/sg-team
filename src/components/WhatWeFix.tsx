23ef3bc02e71e5ae263a92140cce3212cbd79c26
maximiloi <maximiloi@gmail.com>
2026-02-19 19:10:12 +0300

    feat: add what we fix comp

---
+++ src/components/WhatWeFix.tsx
@@ -0,0 +1,18 @@
+export default function WhatWeFix() {
+  return (
+    <section className='text-center my-16 px-4 max-w-3xl mx-auto'>
+      <h2 className='text-2xl sm:text-3xl font-bold text-center mb-8'>
+        Срочно устраняем:
+      </h2>
+      <ul className='text-base sm:text-lg space-y-2 mb-4'>
+        <li>Проваливающуюся педаль тормоза</li>
+        <li>Скрип, стук, вибрацию тормозов</li>
+        <li>Рычащий, грохочущий или прогнивший глушитель</li>
+        <li>Прогар гофры, резонатора, трубы</li>
+      </ul>
+      <p className='text-lg sm:text-xl font-medium text-red-600'>
+        Безопасность и тишина вернутся за 1 визит
+      </p>
+    </section>
+  );
+}
