ed686e75386564cc436bb8c734ee4ca05e347b9d
maximiloi <maximiloi@gmail.com>
2026-02-19 20:22:25 +0300

    feat: add modalcallback

---
+++ src/components/modal/ModalCallBack.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Modal } from '@/components/ui/modal';
+import FormTelegram from '../FormTelegram';
+
+export default function ModalCallBack() {
+  const [open, setOpen] = useState(false);
+  const [showThanks, setShowThanks] = useState(false);
+
+  return (
+    <>
+      <Button className='w-full' size='lg' onClick={() => setOpen(true)}>
+        Заказать обратный звонок
+      </Button>
+      <Modal
+        open={open}
+        onOpenChange={setOpen}
+        title={showThanks ? '' : 'Оставьте ваши данные, мы перезвоним Вам'}
+        description={
+          showThanks ? '' : 'Мы перезвоним Вам в ближайшее рабочее время'
+        }
+        cancelText={showThanks ? '' : 'Отмена'}
+        footer={showThanks ? null : undefined}
+        size='sm'
+      >
+        <FormTelegram
+          buttonText={showThanks ? '' : ' Перезвоните мне 🚀'}
+          onSuccess={() => {
+            setShowThanks(true);
+            setTimeout(() => {
+              setShowThanks(false);
+              setOpen(false);
+            }, 1500);
+          }}
+        />
+      </Modal>
+    </>
+  );
+}
