5b3671067ea8081e33761e4a880d2409a34858cb
maximiloi <maximiloi@gmail.com>
2026-02-19 19:09:14 +0300

    feat: add Header comp

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from './header/Logo';
+
+export default function Header() {
+  return (
+    <header>
+      <div className='flex justify-center'>
+        <Logo />
+      </div>
+    </header>
+  );
+}
