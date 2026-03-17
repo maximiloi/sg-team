e8dce95fd3831909f694a7514d033610ecc8981f
maximiloi <maximiloi@gmail.com>
2026-03-17 11:56:53 +0300

    feat: add redirect auth user

--- src/app/auth/registration/page.tsx
+++ src/app/auth/registration/page.tsx
@@ -1,6 +1,14 @@
+import { auth } from '@/app/auth/authSetup';
 import FormRegistration from '@/components/FormRegistration';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect('/board');
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
